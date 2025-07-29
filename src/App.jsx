1aea26e45496ed0b2508dc2e1959bdbadfad8cd7
marzia <you@example.com>
2025-07-29 12:08:32 +0200

    fix handleValue logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,35 @@ function App() {
   const apiKey = import.meta.env.VITE_API_KEY;
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`;
 
+  //api
+  const [dataApi, setDataApi] = useState(null);
+  // valore dell'imput
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        setDataApi(data);
       });
   }, []);
+
+  const handleValue = (e) => {
+    const searchT = e.target.value;
+    setSearch(searchT);
+    console.log(searchT);
+  };
 
   return (
     <>
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          value={search}
+          onChange={handleValue}
+          placeholder="cerca il tuo film"
+        />
         <button>Cerca</button>
       </div>
       <div>
