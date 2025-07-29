d5dc4ab6843a6e66842c776cc225899a31a2687d
marzia <you@example.com>
2025-07-29 11:38:02 +0200

    import apiKey

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import { useState } from "react";
-
-import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <></>;
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <input type="text" />
+        <button>Cerca</button>
+      </div>
+      <div>
+        <ul>
+          <li>Titolo</li>
+          <li>Titolo originale</li>
+          <li>Lingua</li>
+          <li>Voto</li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
